779872c7435b31c8d216f34c9857604900e1bb08
yashasvi-dwivedi <randomdwivedi@gmail.com>
2025-07-27 19:13:14 +0530

    hlo

---
+++ hello_cargo/target/debug/deps/hello_cargo-e7e2b3e25de0bba2.d
@@ -0,0 +1,5 @@
+/home/random/rust-time/hello_cargo/target/debug/deps/hello_cargo-e7e2b3e25de0bba2: src/main.rs
+
+/home/random/rust-time/hello_cargo/target/debug/deps/hello_cargo-e7e2b3e25de0bba2.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/random/rust-time/hello_cargo/target/debug/hello_cargo: /home/random/rust-time/hello_cargo/src/main.rs
